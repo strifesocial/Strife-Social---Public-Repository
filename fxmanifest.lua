62dbadcfac34353ac217549b3eb18f57b4ffd9b3
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-16 14:25:22 -0400

    Update - fxmanifest.lua
    Fxmanifest.lua updated to be compatible with `lua54`.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Strife Social'
-description 'Adds a /discord command to print the Discord link in the in-game chat'
+author 'Strife Social & Strife Social Developments'
+description 'Adds a /discord command to print the Discord link in the in-game chat. This file is provided under Fair Usage policy.'
+
 version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
+
+lua54 'yes'
